417bf95aad0036986e26cb5d1a4832895ee1226c
Daniel Souza <daniellrs3@hotmail.com>
2020-07-18 13:18:35 -0300

    resolvendo conflitos

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3333
 
 const app = express()
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+})
 
 app.use(express.json())
 app.use(routes)
