8176a947e0c390ab1e4580599520d5b04c74136e
Daniel Souza <daniellrs3@hotmail.com>
2020-07-18 17:06:47 -0300

    ajustando middleware de headers

--- src/server.js
+++ src/server.js
@@ -6,14 +6,13 @@ const PORT = process.env.PORT || 3333
 
 const app = express()
 
+app.use(cors());
+
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    app.use(cors());
     next();
 })
 
